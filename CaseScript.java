6c5185263c062723b66ff409d39a01d1d9c111d5
ashwin-ts69 <ashwin-ts69@csez.zohocorpin.com>
2023-08-29 18:33:01 +0530

    added files

--- CaseScript.java
+++ CaseScript.java
@@ -13,18 +13,15 @@ class A{
 System.setProperty("webdriver.chrome.driver","C:\\Chrome Driver\\chromedriver-win64\\chromedriver.exe");
 WebDriver driver = new ChromeDriver();
 driver.navigate().to("C:\\Users\\ashwin-ts69\\Documents\\Selinium\\Driver Project\\Project 3\\Registration.html");
+
+//hanlding exceptions w.r.t time
 driver.manage().timeouts().implicitlyWait(300, TimeUnit.SECONDS); 
+
 
 // WebDriverWait wait = new WebDriverWait(driver, 20);
 // WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("submitform")));
 
-
-
-
 driver.manage().window().maximize();
-
-
-// driver.setTitle("title1");
 driver.findElement(By.name("username")).sendKeys("Ashwin");
 driver.findElement(By.xpath("//input[@id='dob']")).sendKeys("29/10/2000");
 
@@ -32,19 +29,23 @@ driver.findElement(By.xpath("//input[@id='dob']")).sendKeys("29/10/2000");
 WebElement dropelement=driver.findElement(By.tagName("select"));
 dropelement.findElement(By.xpath("//option[. ='America']")).click();
 
+//Alternative Approach of dropdown element slection
 // WebElement dropdown = driver.findElement(By.id("selenium_commands"));
-//         dropdown.findElement(By.xpath("//option[. = 'Browser Commands']")).click();
+// dropdown.findElement(By.xpath("//option[. = 'Browser Commands']")).click();
 // Select s=new Select(dropelement);
 // s.selectByValue("1");
+
 driver.findElement(By.xpath("//input[@id='email']")).sendKeys("ashwin@zoho.com");
 driver.findElement(By.xpath("//input[@id='pass']")).sendKeys("ashwein@414");
 driver.findElement(By.name("submitform")).click();
 
-// driver.navigate().to("C:\\Users\\ashwin-ts69\\Documents\\Selinium\\Driver Project\\Project 3\\index.html");
+
 driver.findElement(By.xpath("//input[@id='email-name']")).sendKeys("Ashwin");
 driver.findElement(By.xpath("//input[@id='email-pass']")).sendKeys("Ashwin789");
 driver.findElement(By.xpath("//input[@name='submittag']")).click();
 
-
+//opening new window and new tab
+driver.switchTo().newWindow(WindowType.WINDOW);
+driver.switchTo().newWindow(WindowType.TAB);
     }
 }
